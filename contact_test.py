2ba5803eeb27d73d00e59b5e5ea7363813a7879b
eddah <eddahbaty98@gmail.com>
2020-09-08 08:41:38 +0300

    created contact_test.py  so as to carry out all the tests in the User class

---
+++ contact_test.py
@@ -0,0 +1,13 @@
+import unittest  # Importing the unittest module
+from contact import User #Imorting the User class
+
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
