d6a2ab8bdb9e28b09aa0e98fee3f3b511d2596fc
eddah <eddahbaty98@gmail.com>
2020-09-09 14:19:03 +0300

    add functions to delete, create new user, find user, and save new user in the run.py file

--- run.py
+++ run.py
@@ -3,3 +3,27 @@ import random
 from user import User
 from credentials import Credentials
 
+def create_user(name, password):
+    '''
+    function to create a new user
+    '''
+    new_user = User(name, password)
+    return new_user
+
+def save_user(user):
+    '''
+    function to save user
+    '''
+    user.save_user()
+
+def find_user(name):
+    '''
+    function that finds a user by name and returns the user
+    '''
+    return User.find_by_name(name)
+
+def delete_user(user):
+    '''
+    function to delete user
+    '''
+    user.delete_user()
